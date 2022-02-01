8ab352e897b172fe421aab39caba568de4695022
d3da <s2014203@utwente.nl>
2022-02-01 17:12:00 +0100

    Improve directory structure for files generated during training

--- performance_rnn.py
+++ performance_rnn.py
@@ -12,10 +12,6 @@ TODO:
     Model:
         Add validation data to model.fit
         sampling temp / beamsearch etc
-    Input loader:
-        Better dataset shuffling
-            (windows of one track get grouped together)
-
     Misc:
         Remove relative paths, make all relative to __dir__
 
@@ -36,6 +32,9 @@ import time
 import tensorflow as tf
 
 
+PROJECT_DIR = os.path.dirname(__file__)
+
+
 class PerformanceRNNModel(tf.keras.Model):
     """
     seq_length: 512a
@@ -51,13 +50,18 @@ class PerformanceRNNModel(tf.keras.Model):
     LSTM    -> (64, 512a, 512b)
     Dense   -> (64, 512a, 413)  (no activation)
     """
-    def __init__(self, input_loader: PerformanceInputLoader,
-                 model_name: str, checkpoint_dir: str, restore_chkpt: bool,
-                 rnn_units: int = 512, learning_rate: float = 1e-3,
+    def __init__(self,
+                 input_loader: PerformanceInputLoader,
+                 model_name: str = 'performance_rnn',
+                 train_dir: str = PROJECT_DIR,
+                 restore_chkpt: bool = True,
+                 rnn_units: int = 512,
+                 learning_rate: float = 1e-3,
                  dropout: float = 0.0):
         super().__init__(self, name=model_name)
         self.input_loader = input_loader
         self.vocab_size = input_loader.vocab_size
+        self.train_dir = train_dir
 
         # Model layers
         self.lstm1 = tf.keras.layers.LSTM(rnn_units,
@@ -82,19 +86,22 @@ class PerformanceRNNModel(tf.keras.Model):
                      metrics=['accuracy'])
 
         # Setup checkpoints
+        chkpt_dir = os.path.join(train_dir, 'checkpoints', model_name)
         chkpt = tf.train.Checkpoint(model=self, optimizer=self.optimizer)
         self.batch_ctr = tf.Variable(0, trainable=False, dtype=tf.int64)
         self.chkpt_mgr = tf.train.CheckpointManager(
                 chkpt,
-                directory=checkpoint_dir,
-                max_to_keep=50,
-                step_counter=self.batch_ctr,
-                checkpoint_interval=5)
+                directory=chkpt_dir,
+                max_to_keep=100,
+                step_counter=self.batch_ctr)
         if restore_chkpt:
             chkpt.restore(self.chkpt_mgr.latest_checkpoint)
-            print(f'Restored checkpoint (batch {self.batch_ctr.value()})')
+            if self.batch_ctr.value() != 0:
+                print(f'Restored checkpoint (batch {self.batch_ctr.value()})')
+            else:
+                print('Initialized model')
 
-        self.callbacks = [TrainCallback()]
+        self.callbacks = [TrainCallback(train_dir=train_dir)]
 
     def call(self, inputs, training=False, states=None, return_states=False):
         """
@@ -122,20 +129,22 @@ class PerformanceRNNModel(tf.keras.Model):
                         validation_data=None)
 
     @tf.function
-    def generate_step(self, inputs, states):
+    def generate_step(self, inputs, states, temperature):
         predicted_logits, states = self(inputs=inputs, states=states, return_states=True)
         predicted_logits = predicted_logits[:, -1, :]
+        predicted_logits *= temperature
         predicted_categories = tf.random.categorical(predicted_logits, num_samples=1)
         return predicted_categories, states
 
-    # TODO sampling temperature / beam search etc
-    def sample_music(self, start_event_category=0, sample_length=512):
+    # todo provide interface for sampling temperature / beam search etc
+    def sample_music(self, start_event_category=0, sample_length=512, temperature=1.0):
         states = None
         next_category = tf.constant([start_event_category], shape=(1, 1))
+        temperature = tf.constant(temperature, dtype=tf.float32)
         result = [next_category]
         for _ in range(sample_length):
             next_category, states = self.generate_step(
-                    next_category, states)
+                    next_category, states, temperature)
             result.append(next_category)
         result = [r.numpy()[0][0] for r in result]
         return result
@@ -153,25 +162,32 @@ class TrainCallback(tf.keras.callbacks.Callback):
 
     """
     def __init__(self,
-                 base_log_dir='logs',
+                 train_dir,
                  update_freq=25,
                  save_midi_freq=50,
                  save_checkpoint_freq=100,
                  write_steps_per_second=True):
+        super().__init__()
         # TODO global epoch as well?
-        # TODO midi output dir
         # TODO output tf.summary.audio
 
-        project_dir = os.path.dirname(__file__)
-        run_time = time.strftime('%Y.%m.%d-%H:%M:%S', time.localtime())
-        self.log_dir = str(os.path.join(project_dir, base_log_dir, run_time))
+        self.train_dir = train_dir
+        self.sample_dir = os.path.join(train_dir, 'train_samples')
+        if not os.path.exists(self.sample_dir):
+            os.mkdir(self.sample_dir)
+
         self.update_freq = update_freq
         self.save_midi_freq = save_midi_freq
         self.save_checkpoint_freq = save_checkpoint_freq
         self.write_steps_per_second = write_steps_per_second
 
         self._batch_start_time = 0.
-        self.writer = tf.summary.create_file_writer(self.log_dir)
+        self.writer = None
+
+    def on_train_begin(self, logs=None):
+        run_time = time.strftime('%Y.%m.%d-%H:%M:%S', time.localtime())
+        log_dir = str(os.path.join('logs', self.model.name, run_time))
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def on_train_end(self, logs=None):
         self.writer.close()
@@ -194,8 +210,7 @@ class TrainCallback(tf.keras.callbacks.Callback):
             # Generate sample
             music = self.model.sample_music()
             midi = sequence_to_midi(music)
-            midi.save(f'./results/out-b{step}.midi')
-            # print(f'Saved midi (batch {step})')
+            midi.save(os.path.join(self.sample_dir, f'sample_batch_{step}.midi'))
 
         if step % self.save_checkpoint_freq == 0:
             self.model.chkpt_mgr.save()
@@ -207,8 +222,7 @@ class TrainCallback(tf.keras.callbacks.Callback):
 
 
 if __name__ == '__main__':
-    project_dir = os.path.dirname(__file__)
-    dataset_base = os.path.join(project_dir, 'data/maestro-v3.0.0')
+    dataset_base = os.path.join(PROJECT_DIR, 'data/maestro-v3.0.0')
     dataset_csv = os.path.join(dataset_base, 'maestro-v3.0.0.csv')
 
     input_loader = PerformanceInputLoader(
@@ -218,12 +232,17 @@ if __name__ == '__main__':
         min_stride=128,
         max_stride=256,
         batch_size=64,
-        augmentation='aug+')
+        augmentation='aug-')
+
     model = PerformanceRNNModel(
-        input_loader,
-        'performance_rnn',
-        './ckpt',
-        restore_chkpt=True)
+        input_loader=input_loader,
+        model_name='performance_rnn',
+        train_dir=PROJECT_DIR,
+        restore_chkpt=True,
+        rnn_units=512,
+        learning_rate=1e-3,
+        dropout=0.0
+    )
 
     model.train(500)
     sys.exit()
