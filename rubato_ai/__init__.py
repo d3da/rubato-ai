69cecafe1f25cfdbf1fe5569b04e8ec7a62ff0d4
d3da <daandieperink@gmail.com>
2022-05-16 20:38:09 +0200

    Add docstring to __init__
    Change file permissions of __main__.py

--- rubato_ai/__init__.py
+++ rubato_ai/__init__.py
@@ -1,3 +1,9 @@
+"""
+RubatoAI: An implementation of the Music Transformer and Performance RNN.
+
+.. todo:
+    High level overview of the project here
+"""
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
